f1f50c1287db2f3d30aa40246d9e3419a0487d1e
Alexandre Henrique <lopes.alexandre@eaportal.org>
2024-03-31 18:44:30 -0300

    nono commit do CSS e terceiro commit do JS

--- script.js
+++ script.js
@@ -29,9 +29,17 @@ function receberQuantidadePessoas(evento) {
 
 const botoesGorjeta = document.querySelectorAll(".gorjeta input[type='button']")
 botoesGorjeta.forEach(botao => {
-    botao.addEventListener("click", receberPorcentagem)
+    botao.addEventListener("click", receberPorcentagemBotao)
 })
 
-function receberPorcentagem(evento) {
-    console.log(evento.target.value)
+function receberPorcentagemBotao(evento) {
+    botoesGorjeta.forEach(botao => {
+        botao.classList.remove("botao-ativo")
+        
+        if(botao.value === evento.target.value) {
+            botao.classList.add("botao-ativo")
+        }
+    })
+        
+    porcentagem = parseFloat(evento.target.value) / 100
 }
