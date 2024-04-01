75b76d6161eb26b795502a5e87f254a6bde381b9
Alexandre Henrique <lopes.alexandre@eaportal.org>
2024-03-31 22:22:49 -0300

    quarto commit do JS

--- script.js
+++ script.js
@@ -29,10 +29,10 @@ function receberQuantidadePessoas(evento) {
 
 const botoesGorjeta = document.querySelectorAll(".gorjeta input[type='button']")
 botoesGorjeta.forEach(botao => {
-    botao.addEventListener("click", receberPorcentagemBotao)
+    botao.addEventListener("click", receberPorcentagem)
 })
 
-function receberPorcentagemBotao(evento) {
+function receberPorcentagem(evento) {
     botoesGorjeta.forEach(botao => {
         botao.classList.remove("botao-ativo")
         
@@ -41,5 +41,15 @@ function receberPorcentagemBotao(evento) {
         }
     })
         
-    porcentagem = parseFloat(evento.target.value) / 100
+    if(evento.target.value !== "") {
+        porcentagem = parseFloat(evento.target.value) / 100
+    } 
+    else {
+        porcentagem = 0
+    }
+    
+    console.log(porcentagem)
 }
+
+const gorjetaInput = document.querySelector("#outra")
+gorjetaInput.addEventListener("input", receberPorcentagem)
